8f6faa9ad33dae373ea08eee3ca11610ab085b85
lastbyte32 <lastbyte32@gmail.com>
2023-02-14 22:16:38 +0300

    еще тесты

--- internal/server/storage/gauge_test.go
+++ internal/server/storage/gauge_test.go
@@ -1,58 +1,34 @@
 package storage
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-//func TestGaugeMemStorage_All(t *testing.T) {
-//	type fields struct {
-//		values map[string]float64
-//	}
-//	tests := []struct {
-//		name   string
-//		fields fields
-//		want   map[string]float64
-//	}{
-//		// TODO: Add test cases.
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			g := &GaugeMemStorage{
-//				values: tt.fields.values,
-//			}
-//			assert.Equalf(t, tt.want, g.All(), "All()")
-//		})
-//	}
-//}
-//
-//func TestGaugeMemStorage_Get(t *testing.T) {
-//	type fields struct {
-//		values map[string]float64
-//	}
-//	type args struct {
-//		name string
-//	}
-//	tests := []struct {
-//		name   string
-//		fields fields
-//		args   args
-//		want   float64
-//		want1  bool
-//	}{
-//		// TODO: Add test cases.
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			g := &GaugeMemStorage{
-//				values: tt.fields.values,
-//			}
-//			got, got1 := g.Get(tt.args.name)
-//			assert.Equalf(t, tt.want, got, "Get(%v)", tt.args.name)
-//			assert.Equalf(t, tt.want1, got1, "Get(%v)", tt.args.name)
-//		})
-//	}
-//}
+func TestGaugeMemStorage_All(t *testing.T) {
+	g := NewGaugeMemStorage()
+	g.Update("foo_gauge", 99.99)
+	g.Update("bar_gauge", 100.001)
+	all := g.All()
+	assert.Equal(t, 2, len(all), fmt.Sprintf("Expected 2 gauges, but got %d", len(all)))
+
+	assert.Equal(t, 99.99, all["foo_gauge"], fmt.Sprintf("Expected foo_gauge to be 1, but got %f", all["foo_gauge"]))
+	assert.Equal(t, 100.001, all["bar_gauge"], fmt.Sprintf("Expected bar_gauge to be 1, but got %f", all["bar_gauge"]))
+}
+
+func TestGaugeMemStorage_Get(t *testing.T) {
+	s := NewGaugeMemStorage()
+	s.Update("foo_value", 1.1)
+
+	value, exist := s.Get("foo_value")
+	assert.True(t, exist, "Failed to get value for foo_value")
+	assert.Equal(t, 1.1, value, fmt.Sprintf("Value for foo_value should be 1 but got %f", value))
+
+	value, exist = s.Get("non-existent")
+	assert.False(t, exist)
+	assert.Equal(t, float64(0), value)
+}
 
 func TestGaugeMemStorage_Update(t *testing.T) {
 
