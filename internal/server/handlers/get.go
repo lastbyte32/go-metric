a16ed8f4418ec1664ada516f5e07d19a842eb90e
lastbyte32 <lastbyte32@gmail.com>
2023-02-17 21:20:07 +0300

    Упростил отдачу метрики

--- internal/server/handlers/get.go
+++ internal/server/handlers/get.go
@@ -3,35 +3,20 @@ package handlers
 import (
 	"fmt"
 	"github.com/go-chi/chi"
-	"github.com/lastbyte32/go-metric/internal/server/storage"
 	"net/http"
 )
 
 func (h *handler) GetMetric(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("GetMetric")
 
-	name := chi.URLParam(r, "name")
-	found := false
-	value := ""
+	metric, exist := h.metricsStorage.Get(chi.URLParam(r, "name"))
 
-	switch storage.MType(chi.URLParam(r, "type")) {
-	case storage.GAUGE:
-		metric, exist := h.metricsStorage.Get(name)
-		if exist {
-			value = metric.GetGauge()
-			found = true
-		}
-	case storage.COUNTER:
-		metric, exist := h.metricsStorage.Get(name)
-		if exist {
-			value = metric.GetCounter()
-			found = true
-		}
+	if !exist {
+		w.WriteHeader(http.StatusNotFound)
+		return
 	}
 
 	w.Header().Set("Content-Type", "text/plain")
-	if !found {
-		w.WriteHeader(http.StatusNotFound)
-	}
-	w.Write([]byte(value))
+
+	w.Write([]byte(metric.ToString()))
 }
